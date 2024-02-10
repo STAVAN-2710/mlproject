0d8d201a74c6a182b861a6042959a0cb16d1d8b1
Stavan <stavan.sanyal@gmail.com>
2024-02-10 02:26:29 -0600

    Added Model Training and tried Hyper Parameter Tuning

---
+++ src/components/model_trainer.py
@@ -0,0 +1,140 @@
+import os
+import sys
+from dataclasses import dataclass
+
+from catboost import CatBoostRegressor
+from sklearn.ensemble import (
+    AdaBoostRegressor,
+    GradientBoostingRegressor,
+    RandomForestRegressor,
+)
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from xgboost import XGBRegressor
+
+from src.exception import CustomException
+from src.logger import logging
+
+from src.utils import save_object, evaluate_models
+
+@dataclass
+class ModelTrainerConfig:
+    # Path to save the trained model
+    trained_model_file_path = os.path.join("artifacts", "model.pkl")
+
+class ModelTrainer:
+    def   __init__(self):
+        self.model_trainer_config = ModelTrainerConfig()
+
+
+    def initiate_model_trainer(self, train_array, test_array):
+        try:
+            logging.info("Splitting training and test input data")
+
+            # Splitting input arrays into features and targets for both training and testing datasets
+            X_train, y_train, X_test, y_test = (
+                train_array[:,:-1],
+                train_array[:,1],
+                test_array[:,:-1],
+                test_array[:,-1]
+            )
+            
+            # Dictionary of models to evaluate
+            models = {
+                "Random Forest": RandomForestRegressor(),
+                "Decision Tree": DecisionTreeRegressor(),
+                "Gradient Boosting": GradientBoostingRegressor(),
+                "Linear Regression": LinearRegression(),
+                "XGBRegressor": XGBRegressor(),
+                "CatBoosting Regressor": CatBoostRegressor(verbose=False),
+                "AdaBoost Regressor": AdaBoostRegressor(),
+            }
+
+            # Hyperparameters for tuning models
+            # params={
+
+            #     # Parameters are commented out to indicate options for extension or modification
+            #     "Decision Tree": {
+            #         'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+            #         # 'splitter':['best','random'],
+            #         # 'max_features':['sqrt','log2'],
+            #     },
+            #     "Random Forest":{
+            #         # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+            #         # 'max_features':['sqrt','log2',None],
+            #         'n_estimators': [8,16,32,64,128,256]
+            #     },
+            #     "Gradient Boosting":{
+            #         # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+            #         'learning_rate':[.1,.01,.05,.001],
+            #         'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+            #         # 'criterion':['squared_error', 'friedman_mse'],
+            #         # 'max_features':['auto','sqrt','log2'],
+            #         'n_estimators': [8,16,32,64,128,256]
+            #     },
+            #     "Linear Regression":{},
+            #     "XGBRegressor":{
+            #         'learning_rate':[.1,.01,.05,.001],
+            #         'n_estimators': [8,16,32,64,128,256]
+            #     },
+            #     "CatBoosting Regressor":{
+            #         'depth': [6,8,10],
+            #         'learning_rate': [0.01, 0.05, 0.1],
+            #         'iterations': [30, 50, 100]
+            #     },
+            #     "AdaBoost Regressor":{
+            #         'learning_rate':[.1,.01,0.5,.001],
+            #         # 'loss':['linear','square','exponential'],
+            #         'n_estimators': [8,16,32,64,128,256]
+            #     }
+                
+            # }
+
+
+            # Evaluate models and select the best one based on performance metrics
+            logging.info("Evaluating Models")
+            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,
+                                              y_test=y_test,models=models)#,param=params)
+            
+            
+            ## To get best model score from dict
+            logging.info("Getting the best model score")
+            best_model_score = max(sorted(model_report.values()))
+
+            ## To get best model name from dict
+            logging.info("Getting the best model name")
+            best_model_name = list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
+            best_model = models[best_model_name]
+            
+            # Check if the best model meets the minimum performance criteria
+            logging.info("Checking if the best model meets the minimum performance criteria")
+            # if best_model_score<0.6:
+            #     raise CustomException("No best model found")
+            logging.info(f"Best found model on both training and testing dataset")
+            
+            # Save the best model to the specified path
+            logging.info("Saving the best model")
+            save_object(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=best_model
+            )
+
+            # Make predictions with the best model and evaluate its performance
+            predicted=best_model.predict(X_test)
+
+            r2_square = r2_score(y_test, predicted)
+            return r2_square
+
+            
+        except Exception as e:
+            raise CustomException(e,sys)
+
+
+
+
+
