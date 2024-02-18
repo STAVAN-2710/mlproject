eb99003f1c09058c22af266bfb7c176850722f19
Stavan <stavan.sanyal@gmail.com>
2024-02-18 05:10:42 -0600

    Added MLflow

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 ## End to End Machine Learning Project
+
+MLFLOW_TRACKING_URI=https://dagshub.com/stavan.sanyal/mlproject.mlflow \
+MLFLOW_TRACKING_USERNAME=stavan.sanyal \
+MLFLOW_TRACKING_PASSWORD=5f8f14f773ab538c138f3872216f69989b625f85 \
+python script.py
--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -1,6 +1,11 @@
 import os
 import sys
 from dataclasses import dataclass
+import numpy as np
+
+from urllib.parse import urlparse
+import mlflow
+import mlflow.sklearn
 
 from catboost import CatBoostRegressor
 from sklearn.ensemble import (
@@ -8,6 +13,7 @@ from sklearn.ensemble import (
     GradientBoostingRegressor,
     RandomForestRegressor,
 )
+from sklearn.metrics import mean_squared_error,mean_absolute_error
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 from sklearn.neighbors import KNeighborsRegressor
@@ -16,7 +22,6 @@ from xgboost import XGBRegressor
 
 from src.exception import CustomException
 from src.logger import logging
-
 from src.utils import save_object, evaluate_models
 
 @dataclass
@@ -27,6 +32,12 @@ class ModelTrainerConfig:
 class ModelTrainer:
     def   __init__(self):
         self.model_trainer_config = ModelTrainerConfig()
+
+    def eval_metrics(self,actual, pred):
+        rmse = np.sqrt(mean_squared_error(actual, pred))
+        mae = mean_absolute_error(actual, pred)
+        r2 = r2_score(actual, pred)
+        return rmse, mae, r2
 
 
     def initiate_model_trainer(self, train_array, test_array):
@@ -40,7 +51,7 @@ class ModelTrainer:
                 test_array[:,:-1],
                 test_array[:,-1]
             )
-            
+        
             # Dictionary of models to evaluate
             models = {
                 "Random Forest": RandomForestRegressor(),
@@ -110,6 +121,52 @@ class ModelTrainer:
                 list(model_report.values()).index(best_model_score)
             ]
             best_model = models[best_model_name]
+
+            print("This is the best model")
+            print(best_model_name)
+
+            model_names = list(params.keys())
+
+            actual_model=""
+
+            for model in model_names:
+                if best_model_name == model:
+                    actual_model = actual_model + model
+
+            best_params = params[actual_model]
+
+            mlflow.set_registry_uri("https://dagshub.com/stavan.sanyal/mlproject.mlflow")
+            tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+
+
+            # mlflow
+
+            with mlflow.start_run():
+
+                predicted_qualities = best_model.predict(X_test)
+
+                (rmse, mae, r2) = self.eval_metrics(y_test, predicted_qualities)
+
+                mlflow.log_params(best_params)
+
+                mlflow.log_metric("rmse", rmse)
+                mlflow.log_metric("r2", r2)
+                mlflow.log_metric("mae", mae)
+
+
+                # Model registry does not work with file store
+                if tracking_url_type_store != "file":
+
+                    # Register the model
+                    # There are other ways to use the Model Registry, which depends on the use case,
+                    # please refer to the doc for more information:
+                    # https://mlflow.org/docs/latest/model-registry.html#api-workflow
+                    mlflow.sklearn.log_model(best_model, "model", registered_model_name=actual_model)
+                else:
+                    mlflow.sklearn.log_model(best_model, "model")
+
+
+
             
             # Check if the best model meets the minimum performance criteria
             logging.info("Checking if the best model meets the minimum performance criteria")
